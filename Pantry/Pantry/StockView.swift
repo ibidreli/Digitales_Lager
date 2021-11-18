c17a850528f66294529ce29d3fb61a25fd3be4d3
ibidreli <91259212+ibidreli@users.noreply.github.com>
2021-11-18 00:23:08 -0800

    V1

--- Pantry/Pantry/StockView.swift
+++ Pantry/Pantry/StockView.swift
@@ -17,87 +17,111 @@ struct Products: Identifiable {
     
     let name: String
     let picture: URL
+    let productGroup: String
 }
 
 
 struct StockView: View {
     private let database = Database.database().reference()
     @State var listProducts = [Products]()
+    @State var produkt = ""
+
 
     var body: some View {
-        List {
-            ForEach(listProducts) {
-                product in
-                HStack {
-                    Text(product.name)
-                                        
-                    WebImage(url: product.picture)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100)
-                }
-            }.onDelete(perform: delete)
+        VStack{
+            Text(self.produkt)
+        
+            List {
+                ForEach(listProducts) {
+                    product in
+                    HStack {
+                        Text(product.name)
+                                            
+                        WebImage(url: product.picture)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100)
+                    }
+                }.onDelete(perform: delete)
 
-            }.onAppear {
-                getData()
-            }
+                }.onAppear {
+                    getData()
+                }
+        }
             
         
     
         
     }
     private func delete(with indexSet: IndexSet) {
+        
+        indexSet.forEach({
+            index in listProducts
+            print("index::", index)
+            print(listProducts)
+            let productName = listProducts[index].productGroup
+            let ref = database.database.reference().child("group/Elis House/products/" + productName)
+            print(ref)
+            ref.child("picture_path").removeValue()
+            ref.child("product_name").removeValue()
         indexSet.forEach({index in listProducts.remove(at: index)})
+            
+        })
+        
+        
     }
     
-    func deleteProduct(at offsets: IndexSet){
-        print("deleteRow")
-    }
+   
     
     func getData(){
-        print("Button clickt")
         let ref = database.child("group").child("Elis House").child("products")
         ref.child("number_of_products").getData(completion: {error, snapshot in guard
             error == nil else {
-            print(error!.localizedDescription)
+            print("error getting the numbers")
             return;
         }
             let json = snapshot.value as? NSInteger
-            var number_of_products :Int = json!
-            
-            for number_of_product in 1...number_of_products {
-                print(number_of_product)
-                ref.child("product" + String(number_of_product)).getData(completion: {error, second_snapshot in guard
-                    error == nil else {
-                    print(error!.localizedDescription)
-                    return;
-                }
-                    if let dictionary = second_snapshot.value as? NSDictionary {
-                        let picturePath = dictionary["picture_path"] as? String
-                        let productName = dictionary["product_name"] as? String
-                        print(picturePath!)
-                        print(productName!)
-                        
-                        let storageRef = Storage.storage().reference(withPath: picturePath!)
-                        
-                        storageRef.downloadURL { (url, error) in
-                            if error != nil {
-                                print((error?.localizedDescription)!)
-                                return
-                            }
-                            
-                            
-                            if !listProducts.contains(where: {product in product.id == number_of_product}) {
-                                listProducts.append(Products(id: number_of_product, name: productName!, picture: url!))
-                            }
-                        }
-                        
-                        
-                    }
-                print(listProducts)
-                });
+            if json != nil{
                 
+                
+                var number_of_products :Int = json!
+                for number_of_product in 1...number_of_products {
+                    ref.child("product" + String(number_of_product)).getData(completion: {error, second_snapshot in guard
+                        error == nil else {
+                        print("error getting data of specific product")
+                        return;
+                    }
+                        if let dictionary = second_snapshot.value as? NSDictionary {
+                            let picturePath = dictionary["picture_path"] as? String
+                            let productName = dictionary["product_name"] as? String
+                            
+                            
+                            let storageRef = Storage.storage().reference(withPath: picturePath!)
+                            
+                            storageRef.downloadURL { (url, error) in
+                                if error != nil {
+                                    print((error?.localizedDescription)!)
+                                    return
+                                }
+                                
+                                
+                                if !listProducts.contains(where: {product in product.productGroup == "product" + String(number_of_product)}) {
+                                    listProducts.append(Products(id: number_of_product, name: productName!, picture: url!, productGroup: "product" + String(number_of_product)))
+                                }
+                                print("list" , listProducts)
+                            }
+                            
+                            
+                        }
+                    
+                    });
+                    
+                }
             }
+            else {
+                print("error")
+            }
+            
         });
     }
 }
